398ab23bdb9b5279a7baf3be4f1f9170af60ca74
Jiayang Li <admin@0x10c.pw>
2026-04-25 14:09:25 -0400

    Add extended HID mouse gadget

--- src/usb_hid.py
+++ src/usb_hid.py
@@ -105,6 +105,7 @@ class Device:
     KEYBOARD = None  # type: Optional[Device]
     BOOT_KEYBOARD = None  # type: Optional[Device]
     MOUSE = None  # type: Optional[Device]
+    MOUSE_EX = None  # type: Optional[Device]
     BOOT_MOUSE = None  # type: Optional[Device]
     CONSUMER_CONTROL = None  # type: Optional[Device]
     GAMEPAD = None  # type: Optional[Device]
@@ -369,6 +370,55 @@ Device.MOUSE = Device(
     in_report_lengths=[4],
     out_report_lengths=[0],
     name="mouse gadget",
+)
+
+Device.MOUSE_EX = Device(
+    subclass=HID_SUBCLASS_NONE,
+    protocol=HID_PROTOCOL_NONE,
+    descriptor=bytes(
+        (
+            # fmt: off
+            0x05, 0x01,        # Usage Page (Generic Desktop Ctrls)
+            0x09, 0x02,        # Usage (Mouse)
+            0xA1, 0x01,        # Collection (Application)
+            0x09, 0x01,        # Usage (Pointer)
+            0xA1, 0x00,        # Collection (Physical)
+            0x05, 0x09,        # Usage Page (Button)
+            0x19, 0x01,        # Usage Minimum (0x01)
+            0x29, 0x08,        # Usage Maximum (0x08)
+            0x15, 0x00,        # Logical Minimum (0)
+            0x25, 0x01,        # Logical Maximum (1)
+            0x75, 0x01,        # Report Size (1)
+            0x95, 0x08,        # Report Count (8)
+            0x81, 0x02,        # Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+            0x05, 0x01,        # Usage Page (Generic Desktop Ctrls)
+            0x09, 0x30,        # Usage (X)
+            0x09, 0x31,        # Usage (Y)
+            0x16, 0x01, 0x80,  # Logical Minimum (-32767)
+            0x26, 0xFF, 0x7F,  # Logical Maximum (32767)
+            0x75, 0x10,        # Report Size (16)
+            0x95, 0x02,        # Report Count (2)
+            0x81, 0x06,        # Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+            0x09, 0x38,        # Usage (Wheel)
+            0x15, 0x81,        # Logical Minimum (-127)
+            0x25, 0x7F,        # Logical Maximum (127)
+            0x75, 0x08,        # Report Size (8)
+            0x95, 0x01,        # Report Count (1)
+            0x81, 0x06,        # Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+            0x05, 0x0C,        # Usage Page (Consumer)
+            0x0A, 0x38, 0x02,  # Usage (Application Controls - Pan)
+            0x81, 0x06,        # Input (Data,Var,Rel,No Wrap,Linear,Preferred State,No Null Position)
+            0xC0,              # End Collection (Physical)
+            0xC0,              # End Collection (Application)
+            # fmt: on
+        )
+    ),
+    usage_page=0x1,
+    usage=0x02,
+    report_ids=[0x00],
+    in_report_lengths=[7],
+    out_report_lengths=[0],
+    name="extended mouse gadget",
 )
 
 Device.CONSUMER_CONTROL = Device(
